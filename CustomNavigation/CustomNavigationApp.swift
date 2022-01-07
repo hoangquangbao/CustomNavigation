4683d33096ed433a45a987cb7723f9f99bc0bd71
Quang Bao <hoangquangbao.developer@gmail.com>
2022-01-07 11:27:09 +0700

    Initial Commit

---
+++ CustomNavigation/CustomNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomNavigationApp.swift
+//  CustomNavigation
+//
+//  Created by Quang Bao on 07/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct CustomNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
